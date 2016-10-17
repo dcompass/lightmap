4c27742eae1f4712eb107cd001e38b35212fbc64
dcompass <yongz0214@gmail.com>
2016-10-17 19:22:36 +0200

    end commit

---
+++ temp/assets/index.js
@@ -0,0 +1,8 @@
+/*******************************************************************************
+ * Copyright (c) 2016. SkiScool.
+ ******************************************************************************/
+
+module.exports = {
+  CrossStorageClient: require('./client.js'),
+  CrossStorageHub:    require('./hub.js')
+};
